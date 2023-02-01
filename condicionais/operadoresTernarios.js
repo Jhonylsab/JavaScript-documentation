0edf908420208522c7f88df8e421a7844abade0f
Jhony Lima <114432490+Jhonylsab@users.noreply.github.com>
2023-02-01 12:27:56 -0300

    commit exercicios

---
+++ condicionais/operadoresTernarios.js
@@ -0,0 +1,5 @@
+var seuAniversario = true;
+
+var saudacao = (seuAniversario) ? 'Feliz aniversário Sr. Smith - Nós esperamos que você tenha um ótimo dia' : 'Bom dia Sr. Smith.'; 
+
+console.log(saudacao);
