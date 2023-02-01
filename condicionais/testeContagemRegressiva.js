1c2ad69c367d544b702c49001c1d25f381f052fb
Jhony Lima <114432490+Jhonylsab@users.noreply.github.com>
2023-02-01 18:16:29 -0300

    commit exercicio&funcoes

---
+++ condicionais/testeContagemRegressiva.js
@@ -0,0 +1,13 @@
+var i = 10;
+
+while(i >= 0){
+    if(i === 10){
+        console.log('Contagem regressiva ' + i);
+    } else if( i === 0){
+        console.log('Lançar!');
+    } else {
+        console.log(i);
+    }
+
+    i--;
+}
---
+++ funcoes/funcoes1.js
@@ -0,0 +1,7 @@
+var meuTexto = 'Eu sou uma string';
+var novaString = meuTexto.replace('string', 'salsicha');
+console.log(novaString);
+
+var meuArray = ['Eu', 'amo', 'chocolate', 'sapos'];
+var fazerUmaString = meuArray.join(' ');
+console.log(fazerUmaString);
